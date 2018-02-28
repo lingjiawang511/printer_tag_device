4125f01538d229ef2fa5bcb1bb2898e65ae59839
lingjiawang511 <398677675@qq.com>
2018-02-28 15:53:23 +0800

    software:reset the exit pull dowm resistance to pull up

--- printer_mcu/BSP/EXTI/exti.c
+++ printer_mcu/BSP/EXTI/exti.c
@@ -126,7 +126,7 @@ static void  EXTIX9_5_Init(void )
 		RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
 	
 		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9;			 
-		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 		 
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 		 
 		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
 		GPIO_Init(GPIOC, &GPIO_InitStructure);
 
